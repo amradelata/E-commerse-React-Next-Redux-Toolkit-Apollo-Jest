15ab676ddb20cc195f0d3e07924256ca804b5c06
amradelata <amradelata@gmail.com>
2021-09-29 11:34:23 +0200

    :star: new: the first test with jest

---
+++ __tests__/components/Loding-test.js
@@ -0,0 +1,6 @@
+
+'use strict';
+
+import React from 'react';
+import Link from '../Link.react';
+import renderer from 'react-test-renderer';
---
+++ pages/countries-we-ship-to/index.js
@@ -0,0 +1,42 @@
+import { gql } from "@apollo/client";
+import client from "../../apollo-client";
+import styles from "./countries-we-ship-to.module.css";
+export default function CountriesWeShipTo({ countries }) {
+  return (
+    <div className="container is-fullhd">
+      <h2>countries we ship to it </h2>
+      <h5 className={styles.h5}>
+        This data from (countries.trevorblades API) using GraphQL/Apollo
+      </h5>
+      <div className={styles.allCountrys}>
+        {countries.map((country) => (
+          <div key={country.code} className={styles.country}>
+            <h3>{country.name}</h3>
+            <p>
+              {country.code} - {country.emoji}
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+export async function getServerSideProps() {
+  const { data } = await client.query({
+    query: gql`
+      query products {
+        countries {
+          code
+          name
+          emoji
+        }
+      }
+    `,
+  });
+
+  return {
+    props: {
+      countries: data.countries.slice(0, 12),
+    },
+  };
+}
